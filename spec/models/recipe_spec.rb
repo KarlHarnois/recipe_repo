489be2b6db935c622d177f3e00c1233ea48681f1
KarlHarnois <karlrivestharnois@gmail.com>
2022-12-05 20:36:52 -0500

    Create recipe model with name

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Recipe do
+  describe '#name' do
+    let(:recipe) { create :recipe, name: 'Test' }
+
+    it 'has a name' do
+      expect(recipe.name).to eq 'Test'
+    end
+  end
 end
