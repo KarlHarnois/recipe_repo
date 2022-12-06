63aafc605b77750c015650bd2802288e31d8954c
KarlHarnois <karlrivestharnois@gmail.com>
2022-12-06 03:05:45 -0500

    Pluralize application name

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-module RecipeRepository
+module RecipesRepository
   class Application < Rails::Application
     config.load_defaults 7.0
   end
